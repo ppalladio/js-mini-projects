e98044f198f3c8aea0bec84b4b9e0efc10f0a278
Yuxuan <nine.slide@gmail.com>
2022-08-25 15:48:29 +0200

    complete, DRY

--- retro-number-guessing/script.js
+++ retro-number-guessing/script.js
@@ -1,23 +1,63 @@
 'use strict';
 
-const number = Math.trunc(Math.random() * 20)+1
+let number = 20
 let score = 20
-addEventListener('click', _event=>{
+let highScore = 0;
+// number = Number(prompt('how hight you want the score to be'));
+// console.log(number);
+// score = number;
+const genRand = (num) =>{
+   return Math.trunc(Math.random() * num)+1
+};
+const msg = (message) =>{
+    document.querySelector('.message').textContent = message
+}
+const currentScore = (score) => {
+    document.querySelector('.score').textContent = score;
+}
+const numberStyle = (width) => {
+    document.querySelector('.number').style.width = win
+}
+const bodyColor = (color) => {
+    document.querySelector('body').style.backgroundColor = color
+}
+
+
+//** DOM
+
+document.querySelector('.check').addEventListener('click', _event=>{
     const guess = Number(document.querySelector('.guess').value);
+    // 
     if (!guess){
-        document.querySelector('.message').textContent = 'No Number';
+        msg('No Number');
+
+        //** winning in-line style
     } else if  (guess === number){
-        document.querySelector('.message').textContent = 'Number is correct';
-    } else if ( guess > number){
+        msg('Number is correct');
+        bodyColor('#60b347');
+        numberStyle('30rem');
+        if ( score > highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+      
+    } else if ( guess !== number){
+        //** ternary 
         if(score > 0){
-        document.querySelector('.message').textContent = 'Number is too high'
+            msg( guess > number ?'Number is too high' : 'Number is too low');
         score--;
-        document.querySelector('.score').textContent = score;
+        currentScore(score);
     } else {
-        document.querySelector('.message').textContent = 'you lost the game';
-    }}
-    else if (guess < number){
-        document.querySelector('.message').textContent = 'Number is too low'
-        score--;
-        document.querySelector('.score').textContent = score;
- }})
+        msg('you lost the game');
+    }}})
+
+ document.querySelector('.again').addEventListener('click',_event=>{
+    score = 20
+    number = genRand(num)
+    msg('Start Guessing...');
+    currentScore(score);
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    bodyColor('#222');
+    numberStyle('15rem');
+})
